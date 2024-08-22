748e0d4b088a629c4d815c58d5faf125550a703c
Raphael Furtado <fraphaelfurtado@gmail.com>
2024-08-22 17:47:08 -0300

    feat: responsive screen

--- auth/app/(protected)/layout.tsx
+++ auth/app/(protected)/layout.tsx
@@ -6,10 +6,12 @@ interface ProtectedLayoutProps {
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
     return (
-        <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
-    from-sky-400 to-blue-800">
+        <div className="min-h-screen w-full flex flex-col items-center justify-start bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
+        from-sky-400 to-blue-800 p-4 md:p-8 lg:p-16">
             <Navbar />
-            {children}
+            <div className="flex-grow w-full flex flex-col items-center justify-center gap-y-6 md:gap-y-8">
+                {children}
+            </div>
         </div>
     );
 }
--- auth/app/auth/layout.tsx
+++ auth/app/auth/layout.tsx
@@ -1,8 +1,9 @@
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
     return(
-        <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
-    from-sky-400 to-blue-800">
-            {children}
+        <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+            <div className="w-full max-w-md">
+                {children}
+            </div>
         </div>
     );
 }
